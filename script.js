2697bce6527057c68516b6a71a91f16a367a9fda
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2024-05-16 21:04:50 +0100

    Add 16x16 square divs

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const container = document.querySelector("#container");
 
+
+for (let i = 0; i < 16; i ++) {
+
+    for (let i = 0; i < 16; i ++) {
+        let div = document.createElement("div");
+        div.textContent = "Mine";
+        container.appendChild(div);
+    }
+}
+
