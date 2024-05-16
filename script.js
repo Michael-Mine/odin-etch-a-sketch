f1fadd55be671f2332acac10306fd1dd7d80d3f4
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2024-05-16 22:11:27 +0100

    Add button and prompt

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const container = document.querySelector("#container");
 
-for (let i = 0; i < 16; i ++) {
-    for (let i = 0; i < 16; i ++) {
+let gridsLength = 16;
+
+let button = document.createElement("button");
+
+button.textContent = "Click for new Etch-A-Sketch";
+container.appendChild(button);
+
+button.addEventListener("click", () => {
+    gridsLength = prompt("How many squares per side?");
+})
+
+for (let i = 0; i < gridsLength; i ++) {
+    for (let i = 0; i < gridsLength; i ++) {
         let div = document.createElement("div");
-        div.textContent = "Mine";
         container.appendChild(div);
     }
 }
@@ -13,6 +23,6 @@ const divs = document.querySelectorAll("div");
 divs.forEach((div) => {
     div.addEventListener("mouseenter", () => {
         div.style.backgroundColor = "green";
-        console.log("hit");
     })
 })
+
