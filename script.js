06927768a6439de9d67b3b0cda454da1784151e4
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2024-05-17 17:08:28 +0100

    Add removable of old sketch for new sketch

--- script.js
+++ script.js
@@ -7,21 +7,22 @@ function newSketch(gridsLength = 16, gridSize = "62.5px") {
             let div = document.createElement("div");
             div.style.height = gridSize
             div.style.width = gridSize
+            div.classList.add("drawPad")
             container.appendChild(div);
         }
     }
+
+    const divs = document.querySelectorAll("div");
+
+    divs.forEach((div) => {
+    div.addEventListener("mouseenter", () => {
+        div.style.backgroundColor = "green";
+        })
+    })
 }
 
 newSketch();
 
-const divs = document.querySelectorAll("div");
-
-divs.forEach((div) => {
-    div.addEventListener("mouseenter", () => {
-        div.style.backgroundColor = "green";
-
-    })
-})
 const button = document.querySelector("#button");
 
 let button1 = document.createElement("button");
@@ -29,6 +30,9 @@ button1.textContent = "Click for new Etch-A-Sketch";
 button.appendChild(button1);
 
 button1.addEventListener("click", () => {
-    gridsLength = prompt("How many squares per side (maximum 100?");
+    let newGridsLength = prompt("How many squares per side (maximum 100)?");
+    if (newGridsLength > 100) newGridsLength = 100;
+    let newGridSize = (1000 / newGridsLength) + "px";
+    container.replaceChildren();
+    newSketch(newGridsLength, newGridSize);
 })
-
