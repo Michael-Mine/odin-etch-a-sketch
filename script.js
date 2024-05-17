1bd5a782b34b722a6396c555dad11954fb0df8f3
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2024-05-17 15:31:46 +0100

    Move button above

--- script.js
+++ script.js
@@ -2,15 +2,6 @@ const container = document.querySelector("#container");
 
 let gridsLength = 16;
 
-let button = document.createElement("button");
-
-button.textContent = "Click for new Etch-A-Sketch";
-container.appendChild(button);
-
-button.addEventListener("click", () => {
-    gridsLength = prompt("How many squares per side?");
-})
-
 for (let i = 0; i < gridsLength; i ++) {
     for (let i = 0; i < gridsLength; i ++) {
         let div = document.createElement("div");
@@ -23,6 +14,16 @@ const divs = document.querySelectorAll("div");
 divs.forEach((div) => {
     div.addEventListener("mouseenter", () => {
         div.style.backgroundColor = "green";
+
     })
+})
+const button = document.querySelector("#button");
+
+let button1 = document.createElement("button");
+button1.textContent = "Click for new Etch-A-Sketch";
+button.appendChild(button1);
+
+button1.addEventListener("click", () => {
+    gridsLength = prompt("How many squares per side (maximum 100)?");
 })
 
